a07060d295be1016fbc8d0945a040ad1db9e01b5
melissalouie <mwq.louie@gmail.com>
2015-05-17 05:46:49 -0600

    add div slide to work

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,7 @@
 $(function() {
-  console.log('hello');
+  $('#slide').hide();
+  $(':button').on('click', function() {
+    $('#slide').show();
+    $("#slide").animate({"right": "-=300px", "opacity":1}, "slow");
+  });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
-  get '/test' => 'welcome#test'
   resources :contacts
 end
